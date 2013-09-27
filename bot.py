fd7d5d6673c208ca03486b01b3b02107096c8698
AngeZanetti <ange.zanetti@gmail.com>
2013-09-27 22:31:41 +0200

    Add some basic function to the bot

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import socket, ssl
-nick = 'aBarre'
+
+bot = 'aBarre'
 chan = '#lentremise'
 debug = False
 network = 'irc.freenode.net'
@@ -8,14 +9,38 @@ port = 6697
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((network,port))
 irc = ssl.wrap_socket(socket)
-irc.recv (4096)
-irc.send('NICK ' + nick + '\r\n') #Send our Nick(Notice the Concatenation)
+irc.send('NICK ' + bot + '\r\n') #Send our Nick(Notice the Concatenation)
 irc.send('USER AffixBot AffixBot AffixBot :Affix IRC\r\n') #Send User Info to the server
 irc.send('JOIN ' + chan + '\r\n') # Join the pre defined channel
 irc.send('PRIVMSG ' + chan + ' :Hello.\r\n') #Send a Message to the  channel
-while True: #While Connection is Active
-  data = irc.recv (4096) #Make Data the Receive Buffer
-  print data #Print the Data to the console(For debug purposes)
 
-  if data.find('PING') != -1: #If PING is Found in the Data
-    irc.send('PONG ' + data.split()[1] + '\r\n') #Send back a PONG
+# Set up our commands function
+def commands(user,channel,message):
+    if message.find(bot +': mutualab?')!=-1:
+      irc.send('PRIVMSG %s :%s: Mutualab is awesome!!!!!!!\r\n' % (channel,user))
+    elif message.find(bot+': help')!=-1:
+      irc.send('PRIVMSG %s :%s: you can find all the help you need here: http://mutualab.org.\r\n' % (channel,user))
+
+def ping(): # This is our first function! It will respond to server Pings.
+  irc.send("PONG :pingis\n")  
+
+def sendmsg(chan , msg): # This is the send message function, it simply sends messages to the channel.
+  irc.send("PRIVMSG "+ chan +" :"+ msg +"\n") 
+
+def hello(): # This function responds to a user that inputs "Hello Mybot"
+  irc.send("PRIVMSG "+ channel +" :Hello!\n")
+
+while True: #While Connection is Active
+  ircmsg = irc.recv(2048) # receive data from the server
+  ircmsg = ircmsg.strip('\n\r') # removing any unnecessary linebreaks.
+  print(ircmsg) # Here we print what's coming from the server
+
+  if ircmsg.find(' PRIVMSG ')!=-1:
+    user=ircmsg.split('!')[0][1:]
+    channel=ircmsg.split(' PRIVMSG ')[-1].split(' :')[0]
+    commands(user,channel,ircmsg)
+  if ircmsg.find(":Hello "+ bot) != -1: # If we can find "Hello Mybot" it will call the function hello()
+    hello()
+
+  if ircmsg.find("PING :") != -1: # if the server pings us then we've got to respond!
+    ping()
